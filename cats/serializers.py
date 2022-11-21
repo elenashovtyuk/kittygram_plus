729a5786f1efb498d1621437b859b5db66288e2e
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-11-21 13:33:03 +0400

    add serializer for Owner model

--- cats/serializers.py
+++ cats/serializers.py
@@ -2,9 +2,17 @@ from rest_framework import serializers
 
 from .models import Cat
 
-
+# сериализатор для модели Cat
 class CatSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year')
+
+
+# сериализатор для модели Owner
+class OwnerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name')
