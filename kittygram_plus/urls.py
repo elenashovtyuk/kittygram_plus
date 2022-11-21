085f82569430c02e18d02f982b0de92f3742fdda
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-11-22 01:16:31 +0400

    add routers

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,17 @@
 from django.urls import include, path
-
+# импортируем необходимый роутер
 from rest_framework.routers import DefaultRouter
+# импортируем нужные вьюсеты
+from cats.views import CatViewSet, OwnerViewSet
 
-from cats.views import CatViewSet
-
-
+# создаем экземпляр роутера
 router = DefaultRouter()
+# регистрируем эндпоинты, т.е
+# создаем необходимый набор эндпоинтов
 router.register('cats', CatViewSet)
-
+router.register('owners', OwnerViewSet)
+# после регистрации включаем новые эндпоинты
+# в список urlpatterns
 urlpatterns = [
     path('', include(router.urls)),
 ]
