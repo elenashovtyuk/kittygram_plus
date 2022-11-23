ea8ed13a209ab7d3a4486296fe1a5801580ce7bf
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2022-11-24 01:52:05 +0400

    add new path

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 # импортируем необходимый роутер
 from rest_framework.routers import DefaultRouter
 # импортируем нужные вьюсеты
@@ -14,5 +15,8 @@ router.register(r'mycats', LightCatViewSet)
 # после регистрации включаем новые эндпоинты
 # в список urlpatterns
 urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
 ]
